0de66e722e731987263495d0e29b1ec3fec9aee5
xxgpsxx <gps360.infinityx@gmail.com>
2017-08-09 17:34:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
+import feedparser
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, World"
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 
-app.run(port == 5000, debug = true)
+@app.route("/")
+def get_news():
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return """<html>
+    <body>
+    <h1>BBC Headlines</h1>
+    <b>{0}</b><br/>
+    <i>{1}</i> <br/>
+    <p>{2}</p> <br/>
+    </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
+if __name__ == '__main__':
+    app.run(port = 6000, debug = True)
